4358cfe44e3a9f2e908ab8444b64ddc8fc058c7b
unknown <celay.rulz@gmail.com>
2020-04-16 19:03:21 +0200

    Dummy data added

---
+++ tutorial/src/main/java/eu/pontsystems/tutorial/bootstrap/PopulateDb.java
@@ -0,0 +1,73 @@
+package eu.pontsystems.tutorial.bootstrap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import eu.pontsystems.tutorial.entity.User;
+import eu.pontsystems.tutorial.repository.UserRepository;
+
+@Component
+public class PopulateDb implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Autowired
+	private UserRepository ur;
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		
+		Random rd=new Random();
+		
+		User sanyi=new User();
+		sanyi.setName("sanyi");
+		sanyi.setEmail("aa@bb.hu");
+		sanyi.setAge(rd.nextInt(99)+1);
+		
+		User zoli=new User();
+		zoli.setName("zoli");
+		zoli.setEmail("aa@bb.hu");
+		zoli.setAge(rd.nextInt(99)+1);
+		
+		User bela=new User();
+		bela.setName("Béla");
+		bela.setEmail("aaa@bb.hu");
+		bela.setAge(rd.nextInt(99)+1);
+		
+		User judit=new User();
+		judit.setName("Judit");
+		judit.setEmail("aaa@bb.hu");
+		judit.setAge(rd.nextInt(99)+1);
+		
+		User zsofi=new User();
+		zsofi.setName("Zsófi");
+		zsofi.setEmail("aaa@bb.hu");
+		zsofi.setAge(rd.nextInt(99)+1);
+		
+		User aniko=new User();
+		aniko.setName("Anikó");
+		aniko.setEmail("aaa@bb.hu");
+		aniko.setAge(rd.nextInt(99)+1);
+		
+		User albert=new User();
+		albert.setName("Albert");
+		albert.setEmail("aaa@bb.hu");
+		albert.setAge(rd.nextInt(99)+1);
+		List<User> dummys=new ArrayList<User>();
+		
+		dummys.add(sanyi);
+		dummys.add(zoli);
+		dummys.add(bela);
+		dummys.add(judit);
+		dummys.add(zsofi);
+		dummys.add(aniko);
+		dummys.add(albert);
+		
+		ur.saveAll(dummys);
+	}
+
+}
